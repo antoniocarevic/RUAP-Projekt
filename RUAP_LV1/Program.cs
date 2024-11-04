c9e5e04d9599a3f264412b695bf66ec7d86f3fa0
antoniocarevic <antonio.carevic15@gmail.com>
2024-11-04 15:25:40 +0100

    Vracena datoteka na stanje prije indeksa

--- RUAP_LV1/Program.cs
+++ RUAP_LV1/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
+<<<<<<< HEAD
             Console.WriteLine("Pozdrav iz aplikacije od Antonio Carevic 2131321.");
+=======
+            Console.WriteLine("Pozdrav iz aplikacije od Antonio Carevic.");
+>>>>>>> 796ce3f (Dodana pocetna poruka)
         }
     }
 }
