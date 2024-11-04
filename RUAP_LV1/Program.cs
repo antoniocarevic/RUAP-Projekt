3ec1d7e8087d8364cb7a28c009c99b5a394bd9a6
antoniocarevic <antonio.carevic15@gmail.com>
2024-11-04 15:14:42 +0100

    Dodanan

---
+++ RUAP_LV1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RUAP_LV1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Antonio Carevic");
+        }
+    }
+}
