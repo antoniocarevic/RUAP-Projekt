aaa0197a549225691a31424efd36c19f6209bcda
antoniocarevic <antonio.carevic15@gmail.com>
2024-11-04 15:25:40 +0100

    Vracena datoteka na stanje prije indeksa

--- RUAP_LV1/Program.cs
+++ RUAP_LV1/Program.cs
@@ -5,7 +5,11 @@
         static void Main(string[] args)
         {
 <<<<<<< HEAD
+<<<<<<< HEAD
             Console.WriteLine("Pozdrav iz aplikacije od Antonio Carevic 2131321.");
+=======
+            Console.WriteLine("Pozdrav iz aplikacije od Antonio Carevic.");
+>>>>>>> 796ce3f (Dodana pocetna poruka)
 =======
             Console.WriteLine("Pozdrav iz aplikacije od Antonio Carevic.");
 >>>>>>> 796ce3f (Dodana pocetna poruka)
